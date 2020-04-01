953c8585487ad98a9ee7a1d3cfd8d89fbec41673
Paul Adrian Montaño <paul@abovethefray.io>
2020-04-01 14:49:10 +0800

    Initial commit

---
+++ recipe/symlink-extended.php
@@ -0,0 +1,21 @@
+<?php
+/* (c) Anton Medvedev <anton@medv.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Deployer;
+
+desc('Creating symlink to release');
+task('deploy:symlink-extended', function () {
+    if (get('use_atomic_symlink')) {
+        run("mv -T {{deploy_path}}/release {{deploy_path}}/{{public_dir}}");
+    } else {
+        // Atomic symlink does not supported.
+        // Will use simple≤ two steps switch.
+
+        run("cd {{deploy_path}} && {{bin/symlink}} {{release_path}} {{public_dir}}"); // Atomic override symlink.
+        run("cd {{deploy_path}} && rm release"); // Remove release link.
+    }
+});
